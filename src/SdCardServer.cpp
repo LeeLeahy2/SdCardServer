3aa207c08aca5257ab8d3d7f6830ea63d8efef0c
Nathan Seidle <nathan@sparkfun.com>
2022-07-21 09:42:59 -0600

    Update SdCardServer.cpp

--- src/SdCardServer.cpp
+++ src/SdCardServer.cpp
@@ -295,7 +295,7 @@ sdCardSize(
 
     // Get the SD card size
     sdFat->card()->readCSD(&csd); //Card Specific Data
-    sdCardBytes = sd->card()->sectorCount() * 512;
+    sdCardBytes = sdFat->card()->sectorCount();
     sdCardBytes <<= 9;
     sdCardSizeMB = 0.000001 * sdCardBytes;
     return sdCardBytes;
